942b680eaf784aa8ee8b7921049a596a634099aa
Premek Vysoky <premek.vysoky@microsoft.com>
2023-09-11 15:55:22 +0200

    Make it work

--- src/Maestro/Maestro.ContainerApp/Controllers/WeatherForecastController.cs
+++ src/Maestro/Maestro.ContainerApp/Controllers/WeatherForecastController.cs
@@ -5,37 +5,27 @@ using Azure.Identity;
 using Azure.Storage.Queues;
 using Azure.Storage;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
 
 namespace Maestro.ContainerApp.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly QueueServiceClient _queueClient;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, QueueServiceClient queueClient)
     {
         _logger = logger;
+        _queueClient = queueClient;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<IEnumerable<WeatherForecast>> Get()
+    public async Task<IActionResult> Get()
     {
-        // With queue URL and DefaultAzureCredential
-        var client = new QueueClient(new Uri("https://127.0.0.1:10001/devstoreaccount1/new-queue"), new DefaultAzureCredential());
-        await client.SendMessageAsync("Hello, Azure!");
-
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var client = await _queueClient.CreateQueueAsync("new-queue");
+        await client.Value.SendMessageAsync("Hello, Azure!");
+        return Ok();
     }
 }
