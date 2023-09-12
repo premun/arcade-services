cad2bfb3cea40d09189754492501a9627c8ff3c8
dkurepa <dkurepa@microsoft.com>
2023-09-12 18:10:35 +0200

    Move PullRequestActor into the Container proj

---
+++ src/Maestro/Maestro.ContainerApp/Actors/InProgressPullRequest.cs
@@ -0,0 +1,44 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Maestro.Contracts;
+using Microsoft.DotNet.DarcLib;
+
+namespace Maestro.ContainerApp.Actors;
+
+#nullable disable
+
+[DataContract]
+public class InProgressPullRequest : IPullRequest
+{
+    [DataMember]
+    public string Url { get; set; }
+
+    [DataMember]
+    public MergePolicyCheckResult MergePolicyResult { get; set; }
+
+    [DataMember]
+    public List<SubscriptionPullRequestUpdate> ContainedSubscriptions { get; set; }
+
+    [DataMember]
+    public List<SubscriptionPullRequestUpdate> Contained { get; set; }
+
+    [DataMember]
+    public List<DependencyUpdateSummary> RequiredUpdates { get; set; }
+
+    [DataMember]
+    public bool? SourceRepoNotified { get; set; }
+}
+
+[DataContract]
+public class SubscriptionPullRequestUpdate
+{
+    [DataMember]
+    public Guid SubscriptionId { get; set; }
+
+    [DataMember]
+    public int BuildId { get; set; }
+}
