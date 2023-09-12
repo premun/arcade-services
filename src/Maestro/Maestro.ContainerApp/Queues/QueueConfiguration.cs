264901567e88c832c34e0f448e454db61ce36f31
Premek Vysoky <premek.vysoky@microsoft.com>
2023-09-12 13:19:54 +0200

    Add a background service for subscription triggers

---
+++ src/Maestro/Maestro.ContainerApp/Queues/QueueConfiguration.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Maestro.ContainerApp.Queues;
+
+internal static class QueueConfiguration
+{
+    public const string SubscriptionTriggerQueueName = "subscriptions";
+
+    public static void AddAzureQueues(this IServiceCollection services)
+    {
+        services.AddAzureClients(clientBuilder =>
+        {
+            // TODO: This would get replaced with a connection string from builder.Configuration["StorageConnectionString:queue"]
+            clientBuilder.AddQueueServiceClient("UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://host.docker.internal");
+            clientBuilder.ConfigureDefaults(options =>
+            {
+                options.Diagnostics.IsLoggingEnabled = false;
+            });
+        });
+
+        services.TryAddTransient<QueueProducerFactory>();
+    }
+}
