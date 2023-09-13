acfd501bee3cc9438f1d5f2409294d27dad88eb3
Premek Vysoky <premek.vysoky@microsoft.com>
2023-09-13 15:47:52 +0200

    Fix token provider

--- src/Maestro/Maestro.ContainerApp/AzureDevOpsEnvTokenProvider.cs
+++ src/Maestro/Maestro.ContainerApp/AzureDevOpsEnvTokenProvider.cs
@@ -10,16 +10,11 @@ public class AzureDevOpsEnvTokenProvider : IAzureDevOpsTokenProvider
     // one token returned for all accounts
     public Task<string> GetTokenForAccount(string account)
     {
-        return Task.Run(
-            () =>
-            {
-                string? token = Environment.GetEnvironmentVariable(EnvironmentVariables.AZDO_TOKEN_ENV_VAR_NAME);
-                if (token == null)
-                {
-                    throw new Exception($"Environment variable '{EnvironmentVariables.AZDO_TOKEN_ENV_VAR_NAME}' not defined.");
-                }
-                return token;
-            }
-        );
+        string? token = Environment.GetEnvironmentVariable(EnvironmentVariables.AZDO_TOKEN_ENV_VAR_NAME);
+        if (token == null)
+        {
+            throw new Exception($"Environment variable '{EnvironmentVariables.AZDO_TOKEN_ENV_VAR_NAME}' not defined.");
+        }
+        return Task.FromResult(token);
     }
 }
