3b49f820222f2944428856b0297fbfdb637123bb
Tomas Kapin <tokapin@microsoft.com>
2023-09-13 15:27:39 +0200

    DI registration, not runnabe yet

--- src/Maestro/Maestro.ContainerApp/Program.cs
+++ src/Maestro/Maestro.ContainerApp/Program.cs
@@ -7,6 +7,7 @@ using Maestro.ContainerApp.Actors;
 using Maestro.ContainerApp.Queues;
 using Maestro.ContainerApp.Utils;
 using Maestro.Data;
+//using Microsoft.AspNetCore.Authentication;
 using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.GitHub.Authentication;
 using Microsoft.EntityFrameworkCore;
@@ -35,12 +36,16 @@ builder.Services.AddDbContext<BuildAssetRegistryContext>(options =>
 
 
 // transient services
+builder.Services.AddTransient<Microsoft.Extensions.Internal.ISystemClock, SystemClockInternal>();
+builder.Services.AddTransient<IGitHubAppTokenProvider, GitHubAppTokenProvider>();
+builder.Services.AddTransient<IInstallationLookup, InMemoryCacheInstallationLookup>();
 builder.Services.AddTransient<IGitHubTokenProvider, GitHubTokenProvider>();
 builder.Services.AddTransient<IAzureDevOpsTokenProvider, AzureDevOpsTokenProvider>();
 builder.Services.AddTransient<DarcRemoteMemoryCache>();
 builder.Services.AddTransient<TemporaryFiles>();
 builder.Services.AddTransient<IBarClient, MaestroBarClient>();
 builder.Services.AddTransient<ILocalGit, LocalGit>();
+builder.Services.AddTransient<IRemoteFactory, DarcRemoteFactory>();
 
 // singleton services
 builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.GetConnectionString("Redis")));
