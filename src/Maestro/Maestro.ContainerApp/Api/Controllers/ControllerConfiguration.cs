d76d492ed7f73e09fb25a40dad825fde168b7fb1
Premek Vysoky <premek.vysoky@microsoft.com>
2023-09-13 16:22:06 +0200

    Add MergePolicyEvaluator

--- src/Maestro/Maestro.ContainerApp/Api/Controllers/ControllerConfiguration.cs
+++ src/Maestro/Maestro.ContainerApp/Api/Controllers/ControllerConfiguration.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Maestro.AzureDevOps;
+using Maestro.ContainerApp.Actors;
 using Microsoft.DotNet.DarcLib;
 using Microsoft.DotNet.GitHub.Authentication;
 using Microsoft.DotNet.Internal.Logging;
@@ -15,20 +16,21 @@ internal static class ControllerConfiguration
 {
     public static void AddControllerConfigurations(this WebApplicationBuilder builder)
     {
-        builder.Services.AddTransient<IGitHubClientFactory, GitHubClientFactory>();
-        builder.Services.AddTransient<IGitHubAppTokenProvider, GitHubAppTokenProvider>();
-        builder.Services.AddTransient<IInstallationLookup, InMemoryCacheInstallationLookup>();
-        builder.Services.AddTransient<IGitHubTokenProvider, GitHubTokenProvider>();
-        builder.Services.AddTransient<IAzureDevOpsTokenProvider, AzureDevOpsTokenProvider>();
         builder.Services.AddTransient<DarcRemoteMemoryCache>();
-        builder.Services.AddTransient<TemporaryFiles>();
+        builder.Services.AddTransient<ExponentialRetry>();
+        builder.Services.AddTransient<IAzureDevOpsTokenProvider, AzureDevOpsTokenProvider>();
         builder.Services.AddTransient<IBarClient, MaestroBarClient>();
+        builder.Services.AddTransient<IGitHubAppTokenProvider, GitHubAppTokenProvider>();
+        builder.Services.AddTransient<IGitHubClientFactory, GitHubClientFactory>();
+        builder.Services.AddTransient<IGitHubTokenProvider, GitHubTokenProvider>();
+        builder.Services.AddTransient<IInstallationLookup, InMemoryCacheInstallationLookup>();
         builder.Services.AddTransient<ILocalGit, LocalGit>();
+        builder.Services.AddTransient<IMergePolicyEvaluator, MergePolicyEvaluator>();
         builder.Services.AddTransient<IRemoteFactory, DarcRemoteFactory>();
         builder.Services.AddTransient<ISystemClock, SystemClock>();
-        builder.Services.AddTransient<ExponentialRetry>();
         builder.Services.AddTransient<IVersionDetailsParser, VersionDetailsParser>();
         builder.Services.AddTransient<OperationManager>();
+        builder.Services.AddTransient<TemporaryFiles>();
         builder.Services.AddKustoClientProvider("Kusto");
         builder.Services.AddGitHubTokenProvider();
     }
