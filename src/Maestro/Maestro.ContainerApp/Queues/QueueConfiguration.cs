e484e98a1652266b832e5ab8a3ed2f26e56da4b5
Premek Vysoky <premek.vysoky@microsoft.com>
2023-09-14 17:19:35 +0200

    Implement daily check queue processor

--- src/Maestro/Maestro.ContainerApp/Queues/QueueConfiguration.cs
+++ src/Maestro/Maestro.ContainerApp/Queues/QueueConfiguration.cs
@@ -14,6 +14,7 @@ internal static class QueueConfiguration
     {
         builder.Services.AddTransient<BuildCoherencyInfoQueueProcessor>();
         builder.Services.AddTransient<StartSubscriptionUpdateQueueProcessor>();
+        builder.Services.AddTransient<CheckDailySubscriptionsQueueProcessor>();
 
         builder.Services.AddAzureClients(clientBuilder =>
         {
